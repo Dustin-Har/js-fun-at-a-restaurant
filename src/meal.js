0552059fe005882cf459b233704afac605cbc4bd
Dustin <>
2021-02-06 21:20:45 -0500

    add meal.js work

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,48 @@
-function nameMenuItem() {
-
+function nameMenuItem(name) {
+  return `Delicious ${name}`
 }
+
+function createMenuItem(name, price, type) {
+  return {
+    name: name,
+    price: price,
+    type: type,
+  }
+}
+
+function addIngredients(ingredient, ingredients){
+  if(ingredients.includes(ingredient)){
+    return ingredients;
+  }else{
+    return ingredients.push(ingredient);
+  }
+}
+
+function formatPrice(price){
+  return `$${price}`;
+}
+
+function decreasePrice(initialPrice){
+  return initialPrice * .9;
+}
+
+function createRecipe(title, ingredients, type){
+  return {
+    title: title,
+    ingredients: ingredients,
+    type: type,
+  };
+}
+
+
+
 
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
