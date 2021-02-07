cfd0f9533e3396c0d551307086804f99dcfa8605
Dustin <>
2021-02-07 17:45:09 -0500

    add work to order

--- src/order.js
+++ src/order.js
@@ -1,7 +1,37 @@
+function takeOrder(orderNumber, deliveryList){
+  for (var i = 0; i <= deliveryList.length; i++){
+    if (deliveryList.length <= 2){
+      return deliveryList.push(orderNumber);
+    }
+  }
+};
+
+function refundOrder(index, deliveryList){
+  if(index === 1){
+   deliveryList.splice(0, 1);
+  }else{
+   deliveryList.splice(1, 1);
+  }
+};
+
+function listItems(deliveryList){
+return `${deliveryList[0].item}, ${deliveryList[1].item}, ${deliveryList[2].item}`;
+};
+
+// function searchOrder(deliveryList){
+//   if (deliveryList[0].item === true && deliveryList[1].item === false){
+//     return deliveryList[0].item;
+//   }if else (deliveryList[0].item === false && deliveryList[1].item === true){
+//     return deliveryList[1].item;
+//   }else{
+//     return "sushi";
+//   }
+// };
+
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  //searchOrder
 }
